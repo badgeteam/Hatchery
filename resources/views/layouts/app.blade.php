047dda95f8ea5a36c2ee19923263d8f4e308794d
Anne Jan Brouwer <brouwer@annejan.com>
2017-06-12 18:04:41 +0200

    Menu right

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,9 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li>
+                            <a href="{{ route('projects.index') }}">Eggs</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -54,9 +56,6 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li>
-                                <a href="{{ route('projects.index') }}">Eggs</a>
-                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
