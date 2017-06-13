75854a4adb3f74d6485ad27add4c751d31dcd6e6
Anne Jan Brouwer <brouwer@annejan.com>
2017-06-13 09:48:05 +0200

    Secure-Headers

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Bepsvpt\SecureHeaders\SecureHeadersServiceProvider::class,
     ],
 
     /*
