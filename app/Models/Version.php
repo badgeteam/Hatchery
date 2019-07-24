2b7b6e939e5d548bc1f6d2db42d30627d6dcc7b3
Anne Jan Brouwer <brouwer@annejan.com>
2019-07-24 15:14:16 +0200

    Quick fix

--- app/Models/Version.php
+++ app/Models/Version.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
