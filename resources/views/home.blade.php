c18d1880e12dc293039a0a31eef615feb3bdeec3
Anne Jan Brouwer <brouwer@annejan.com>
2019-08-29 21:06:52 +0200

    Per-badge stats

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,16 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
-                    Contributors: {{$users}}
-                    Eggs: {{$projects}}
+                    <div>
+                        Contributors: {{$users}}
+                        Eggs: {{$projects}}
+                    </div>
+                    <div>
+                        @foreach(App\Models\Badge::all() as $badge)
+                            {{ $badge->name }}: {{ $badge->projects->count() }}
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row">
-            <div class="panel panel-compact">
-                <div class="panel-body">
-
+		<div class="panel panel-compact">
+			<div class="panel-body">
 		    @if (Route::has('login'))
 			<div class="pull-right links">
 			    @if (Auth::check())
@@ -18,56 +17,61 @@
 		    @endif
 
 		    <div class="content text-center">
-			<div class="title m-b-md">
-			    <h1 class="hatcher"><span class="hidden-xs">Badge.team</span> {{ config('app.name', 'Laravel') }}</h1>
-			</div>
-			<div>
-			    Contributors: {{$users}}
-			    Eggs: {{$projects}}
-			</div>
-			<div class="spacer col-md-12 hidden-xs"></div>
-			<div class="links">
-			    <a href="https://wiki.badge.team/MicroPython">Wiki Coding Help</a>
-			    <a href="https://github.com/badgeteam/">GitHub</a>
-			    <a href="https://twitter.com/SHA2017Badge">Twitter</a>
-			</div>
-			<div class="spacer col-md-12 hidden-xs"></div>
-			{{ Form::select('badge_id', \App\Models\Badge::pluck('name', 'slug')->reverse()->prepend('Choose a badge model', ''), $badge, ['id' => 'badge']) }}
+				<div class="title m-b-md">
+					<h1 class="hatcher"><span class="hidden-xs">Badge.team</span> {{ config('app.name', 'Laravel') }}</h1>
+				</div>
+				<div>
+					Contributors: {{$users}}
+					Eggs: {{$projects}}
+				</div>
+				<div>
+					@foreach(App\Models\Badge::all() as $badge)
+						{{ $badge->name }}: {{ $badge->projects->count() }}
+					@endforeach
+				</div>
+				<div class="spacer col-md-12 hidden-xs"></div>
+				<div class="links">
+					<a href="https://wiki.badge.team/MicroPython">Wiki Coding Help</a>
+					<a href="https://github.com/badgeteam/">GitHub</a>
+					<a href="https://twitter.com/SHA2017Badge">Twitter</a>
+				</div>
+				<div class="spacer col-md-12 hidden-xs"></div>
+				{{ Form::select('badge_id', \App\Models\Badge::pluck('name', 'slug')->reverse()->prepend('Choose a badge model', ''), $badge, ['id' => 'badge']) }}
 
-			<table class="table table-condensed">
-			    <thead>
-			    <tr>
-					<th>Name</th>
-					<th>Revision</th>
-					<th>Size of zip</th>
-					<th>Size of content</th>
-					<th>Category</th>
-					<th>Last release</th>
-			    </tr>
-			    </thead>
-			    <tbody>
-			    @forelse($published as $project)
-				<tr>
-				    <td><a href="{{ route('projects.show', ['project' => $project->slug]) }}">{{ $project->name }}</a></td>
-				    <td>{{ $project->revision }}</td>
-				    <td>{{ $project->size_of_zip }}</td>
-				    <td>{{ $project->size_of_content }}</td>
-					<td>{{ $project->category }}</td>
+				<table class="table table-condensed">
+						<thead>
+					<tr>
+						<th>Name</th>
+						<th>Revision</th>
+						<th>Size of zip</th>
+						<th>Size of content</th>
+						<th>Category</th>
+						<th>Last release</th>
+					</tr>
+					</thead>
+					<tbody>
+					@forelse($published as $project)
+					<tr>
+						<td><a href="{{ route('projects.show', ['project' => $project->slug]) }}">{{ $project->name }}</a></td>
+						<td>{{ $project->revision }}</td>
+						<td>{{ $project->size_of_zip }}</td>
+						<td>{{ $project->size_of_content }}</td>
+						<td>{{ $project->category }}</td>
 
-					<td>{{ $project->versions()->published()->get()->last()->updated_at->diffForHumans() }}</td>
-				</tr>
-			    @empty
-				<tr><td>No published eggs</td></tr>
-			    @endforelse
-                    </tbody>
-                </table>
-				@if ($badge)
-					{{ $published->appends(['badge' => $badge])->links() }}
-				@else
-					{{ $published->links() }}
-				@endif
-            </div>
-        </div>
+						<td>{{ $project->versions()->published()->get()->last()->updated_at->diffForHumans() }}</td>
+					</tr>
+					@empty
+					<tr><td>No published eggs</td></tr>
+					@endforelse
+						</tbody>
+					</table>
+					@if ($badge)
+						{{ $published->appends(['badge' => $badge])->links() }}
+					@else
+						{{ $published->links() }}
+					@endif
+				</div>
+			</div>
         </div>
     </div>
 </div>
