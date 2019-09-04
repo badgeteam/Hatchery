d711f60ee1f2db4ff6525fa17cb328d29dfa7c66
Anne Jan Brouwer <brouwer@annejan.com>
2019-09-04 12:11:25 +0200

    Let's do more testing

--- config/database.php
+++ config/database.php
@@ -39,12 +39,6 @@ return [
             'prefix'   => '',
         ],
 
-        'sqlite_testing' => [
-            'driver'   => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('testing.sqlite')),
-            'prefix'   => '',
-        ],
-
         'mysql' => [
             'driver'      => 'mysql',
             'host'        => env('DB_HOST', '127.0.0.1'),
