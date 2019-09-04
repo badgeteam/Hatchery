bad7076448a12256b7c5240599ff397e2fca7eb5
Anne Jan Brouwer <brouwer@annejan.com>
2019-09-04 13:00:23 +0200

    Hardcoded testing creds updated

--- config/database.php
+++ config/database.php
@@ -69,10 +69,10 @@ return [
 
         'testing' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
-            'username'  => env('DB_TEST_USERNAME', 'homestead'),
-            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'host'      => env('DB_TEST_HOST', '127.0.0.1'),
+            'database'  => env('DB_TEST_DATABASE', 'badgeware_test'),
+            'username'  => env('DB_TEST_USERNAME', 'root'),
+            'password'  => env('DB_TEST_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
