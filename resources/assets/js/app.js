815397889d54ec17ab9a43df8b9c2be5e4721653
Anne Jan Brouwer <brouwer@annejan.com>
2020-02-05 21:42:51 +0100

    install sw

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,8 @@
 /* global require */
 
-// if('serviceWorker' in navigator) {
-// 	navigator.serviceWorker.register('/sw.js');
-// }
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js');
+}
 
 /**
  * First we will load all of this project's JavaScript dependencies which
