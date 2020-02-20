933b7bc79fc8d275e7ece8e9701f6c5a8fca0964
Anne Jan Brouwer <brouwer@annejan.com>
2020-02-20 16:10:23 +0100

    Public routing fix

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', 'PublicController@index')->name('splash');
 Route::get('badge/{badge}', 'PublicController@badge')->name('badge');
 Route::get('projects', 'ProjectsController@index')->name('projects.index');
+Route::get('projects/{project}', 'ProjectsController@show')->name('projects.show');
 Route::post('search', 'ProjectsController@index')->name('projects.search');
 Route::get('search', 'ProjectsController@index')->name('projects.search');
 
@@ -30,7 +31,7 @@ Route::middleware(['auth'])->group(function () {
 Route::middleware(['auth', 'webauthn', '2fa'])->group(function () {
     Route::get('home', 'HomeController@index')->name('home');
 
-    Route::resource('projects', 'ProjectsController')->except('index');
+    Route::resource('projects', 'ProjectsController')->except(['index', 'show']);
     Route::get('projects/{project}/rename', 'ProjectsController@renameForm')->name('projects.rename');
     Route::post('projects/{project}/rename', 'ProjectsController@rename')->name('projects.rename');
     Route::post('notify/{project}', 'ProjectsController@notify')->name('projects.notify');
