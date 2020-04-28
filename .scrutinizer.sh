7878c1ed46a7b4516f2a9edbf0390f1ccda3930e
Anne Jan Brouwer <brouwer@annejan.com>
2020-04-29 01:13:01 +0200

    Whoopsie

--- .scrutinizer.sh
+++ .scrutinizer.sh
@@ -1,3 +1,2 @@
 docker exec -it build_laravel_1 vendor/bin/phpunit --coverage-clover=coverage.clover
-docker cp build_laravel_1:/app/coverage.clover
-pwd
+docker cp build_laravel_1:/app/coverage.clover coverage.clover
