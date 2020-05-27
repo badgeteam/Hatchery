cc016cc0d1237e8d359b059981b761226dfb21fd
Anne Jan Brouwer <brouwer@annejan.com>
2020-05-27 10:37:44 +0200

    Added Pest

---
+++ tests/Helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Tests\TestCase;
+
+/**
+ * Set the currently logged in user for the application.
+ * @param Authenticatable $user
+ * @param string|null $driver
+ * @return TestCase
+ */
+function actingAs(Authenticatable $user, string $driver = null): TestCase
+{
+    return test()->actingAs($user, $driver);
+}
