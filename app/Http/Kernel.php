1ed88653e0f9c26dbf86bb2120c78615527f788f
Anne Jan Brouwer <brouwer@annejan.com>
2020-05-28 16:20:10 +0200

    Cors 2.0 cleanup and circleci results

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -83,6 +83,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Bepsvpt\SecureHeaders\SecureHeadersMiddleware::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
--- tests/Helpers.php
+++ tests/Helpers.php
@@ -5,8 +5,10 @@ use Tests\TestCase;
 
 /**
  * Set the currently logged in user for the application.
+ *
  * @param Authenticatable $user
- * @param string|null $driver
+ * @param string|null     $driver
+ *
  * @return TestCase
  */
 function actingAs(Authenticatable $user, string $driver = null): TestCase
